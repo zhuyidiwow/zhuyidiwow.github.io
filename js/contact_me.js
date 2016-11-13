fb806bfe886a2791a1a7e3f7be66bc28c9feb4ce
AndyZhu <zhuyidiwow@gmail.com>
2016-11-14 03:13:39 +0800

    update contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -51,6 +51,7 @@ $(function() {
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
+
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
