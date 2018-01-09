773814bf06f461514df24224c153fa1c9a9e4f41
Yidi Zhu <yidizhu@halitegames.com>
2018-01-08 22:55:01 -0800

    New layout

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    
+    
+    select($("#featured"));
+    $(".featured").fadeIn();
+
+    $("#featured").click(function() {
+        select($("#featured"));
+        $(".featured").fadeIn();
+    });
+
+    $("#all").click(function() {
+        select($("#all"));
+        $(".item-wrapper").fadeIn();
+    });
+
+    $("#vr").click(function() {
+        select($("#vr"));
+        $(".vr").fadeIn();
+    });
+
+    $("#pc").click(function() {
+        select($("#pc"));
+        $(".pc").fadeIn();
+    });
+
+    $("#mobile").click(function() {
+        select($("#mobile"));
+        $(".mobile").fadeIn();
+    });
+});
+
+
+var select = function(e) {
+    resetAll();
+    
+    $(e).css("background", "#3dbee2");
+    $(e).css("border", "1px solid #3dbee2");
+    $(e).css("color", "snow");
+
+    $(".item-wrapper").hide();
+}
+
+var reset = function(e) {
+    
+    $(e).css("background", "white");
+    $(e).css("border", "1px solid #9a9a9a");
+    $(e).css("color", "#7a7a7a");
+}
+
+var resetAll = function() {
+    reset($("#all"));
+    reset($("#featured"));
+    reset($("#vr"));
+    reset($("#pc"));
+    reset($("#mobile"));
+}
